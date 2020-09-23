5747b6768f9c875c36576d6135b78a62620c2add
Han Sangwook <9967han@naver.com>
2020-09-23 10:18:04 +0900

    Update 14889.cpp

--- BaekJoon/14889.cpp
+++ BaekJoon/14889.cpp
@@ -1,63 +1,56 @@
-#include<stdio.h>
+#include<iostream>
+#include<cstring>
 #include<algorithm>
-#include<math.h>
-#include<vector>
+#include<cmath>
 using namespace std;
-vector<int> start;
-vector<int> link;
-void input();
-void DFS(int curNode, int curCnt);
+void DFS(int idx, int cnt);
+int getMinDiff();
+int N, map[21][21], ans = 987654321;
+int visit[21], selected[11], unselected[11];
 
-int N;
-int map[21][21];
-int check[21];
-int ans=987654321;
-
-int main() {
-	input();
-	DFS(0, 0);
-	printf("%d", ans);
+int main(){
+    memset(visit, 0, sizeof(visit));
+    cin >> N;
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=N; j++){
+            cin >> map[i][j];
+        }
+    }
+    DFS(1, 1);
+    cout << ans << endl;
 }
 
-void input() {
-	scanf("%d", &N);
-	for(int i=1; i<=N; i++) {
-		for(int j=1; j<=N; j++) {
-			scanf("%d", &map[i][j]);
-		}
-	}
+void DFS(int idx, int cnt){
+    if(cnt == N/2+1) {
+        for(int i=1, j=1; i<=N; i++){
+            if(!visit[i]) {
+                unselected[j] = i;
+                j++;
+            }
+        }
+        ans = min(ans, getMinDiff());
+        return;
+    }
+
+    for(int i=idx; i<=N; i++){
+        if(visit[i]) continue;
+        visit[i] = 1;
+        selected[cnt] = i;
+        DFS(i, cnt+1);
+        visit[i] = 0;
+    }
+    return;
 }
 
-void DFS(int curNode, int curCnt){
-	// printf("curNode : %d, curCnt : %d\n", curNode, curCnt);
-	if (curCnt == N/2) {
-		for(int i=1; i<=N; i++) {
-			if(check[i]) start.push_back(i);
-			else link.push_back(i);
-		}
-		int len = start.size();
-		int startScore = 0;
-		int linkScore = 0;
-		for(int i=0; i<len; i++) {//1 2 3  4 5 6
-			for(int j=i+1; j<len; j++) {
-				startScore += map[start[i]][start[j]];
-				startScore += map[start[j]][start[i]];
-				linkScore += map[link[i]][link[j]];
-				linkScore += map[link[j]][link[i]];
-			}
-		}
-		start.clear();
-		link.clear();
-		// printf("start : %d, link : %d\n", startScore, linkScore);
-		ans = min(ans, abs(startScore-linkScore));
-		return;
-	} 
-
-	for(int i=curNode+1; i<=N; i++) {
-		if(check[i] == 0) {
-			check[i] = 1;
-			DFS(i, curCnt+1);
-			check[i] = 0;
-		}
-	}
+int getMinDiff(){
+    int sumSel = 0, sumUnSel = 0;
+    for(int i=1; i<=N/2; i++){
+        for(int j=i+1; j<=N/2; j++){
+            sumSel += map[selected[i]][selected[j]];
+            sumSel += map[selected[j]][selected[i]];
+            sumUnSel += map[unselected[i]][unselected[j]];
+            sumUnSel += map[unselected[j]][unselected[i]];
+        }
+    }
+    return abs(sumSel-sumUnSel);
 }
