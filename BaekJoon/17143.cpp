c994132dcf57bed461002b19d0f7a412b4153bfd
Han Sangwook <9967han@naver.com>
2020-09-25 14:15:48 +0900

    Update 17143.cpp

--- BaekJoon/17143.cpp
+++ BaekJoon/17143.cpp
@@ -1,119 +1,104 @@
 #include<iostream>
-#include<vector>
-#include<queue>
 #include<algorithm>
+#include<vector>
 #include<cstring>
 using namespace std;
-void input();
-void copyMap();
-void printMap();
+void catchShark(int y);
+void moveShark();
+void setMap();
 struct shark{
-	int x; int y; int s; int d; int z;
+    int x, y, s, d, z;
+    bool die;
 };
-shark moveShark(shark S);
-
-int R, C, M;
-int map[101][101], temp[101][101];
-queue<shark> q, tempq;
-int ans = 0;
-int dx[] = {0, -1, 1, 0, 0};
-int dy[] = {0, 0, 0, 1, -1};
-
+vector<shark> sharks;
+int map[101][101];
+int N, M, K, ans=0;
 int main(){
-	input();
-	for(int curPos=1; curPos<=C; curPos++){
-		// printMap();
-		for(int j=1; j<=R; j++){
-			if(map[j][curPos]){ // 상어가 있으면
-				ans+=map[j][curPos]; // 잡음
-				map[j][curPos] = 0;
-				break;
-			}
-		}
-
-		while(!q.empty()){ // C * M = C^3 
-			shark curShark = q.front(); q.pop();
-			if(map[curShark.x][curShark.y] == curShark.z) {
-				tempq.push(moveShark(curShark)); // C^4  = 10^8 = 10000000
-			}
-		}
-
-		while(!tempq.empty()){ // C^3
-			shark curShark = tempq.front(); tempq.pop();
-			if(curShark.z) q.push(curShark);
-		}
-		copyMap();
-	}
-	cout << ans;
+    cin >> N >> M >> K;
+    int r, c, s, d, z;
+    for(int k=1; k<=K; k++){
+        cin >> r >> c >> s >> d >> z;
+        if(d == 1 || d == 2) s %= (2*N-2);
+        if(d == 3 || d == 4) s %= (2*M-2);
+        sharks.push_back({r, c, s, d, z, false});
+    }
+    for(int i=1; i<=M; i++){
+        setMap();
+        catchShark(i);
+        moveShark();
+    }
+    cout << ans;
 }
 
-void printMap(){
-	cout << "cur ans : " << ans << endl;
-	for(int i=1; i<=R; i++){
-		for(int j=1; j<=C; j++){
-			cout << map[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-	return;
+void setMap(){
+    memset(map, 0, sizeof(map));
+    for(int i=0; i<sharks.size(); i++){
+        if(sharks[i].die) continue;
+        int cx = sharks[i].x, cy = sharks[i].y;
+        if(!map[cx][cy]) map[cx][cy] = sharks[i].z;
+        else if(map[cx][cy] > sharks[i].z) sharks[i].die = true;
+        else {
+            for(int j=0; j<sharks.size(); j++) {
+                if(sharks[j].die) continue;
+                if(cx == sharks[j].x && cy == sharks[j].y && map[cx][cy] == sharks[j].z) {
+                    sharks[j].die = true;
+                    break;
+                }
+            }
+            map[cx][cy] = sharks[i].z;
+        }
+    }
+    return;
 }
 
-void copyMap(){
-	for(int i=1; i<=R; i++){
-		for(int j=1; j<=C; j++){
-			map[i][j] = temp[i][j];
-		}
-	}
-	memset(temp, 0, sizeof(temp));
-	return;
+void moveShark(){ //d 1 2 3 4 : 위 아래 오른쪽 왼쪽
+    for(int i=0; i<sharks.size(); i++){
+        if(sharks[i].die) continue;
+        int cx = sharks[i].x, cy = sharks[i].y;
+        for(int j=1; j<=sharks[i].s; j++){
+            if(sharks[i].d == 1) {
+                cx--;
+                if(cx < 1) {
+                    cx = 2;
+                    sharks[i].d = 2;
+                }
+            } else if(sharks[i].d == 2) {
+                cx++;
+                if(cx > N) {
+                    cx = N-1;
+                    sharks[i].d = 1;
+                }
+            } else if(sharks[i].d == 3) {
+                cy++;
+                if(cy > M) {
+                    cy = M-1;
+                    sharks[i].d = 4;
+                }
+            } else {
+                cy--;
+                if(cy < 1) {
+                    cy = 2;
+                    sharks[i].d = 3;
+                }
+            }
+        }
+        sharks[i].x = cx, sharks[i].y = cy;
+    }
+    return;
 }
 
-void input(){
-	cin >> R >> C >> M;
-	int r, c, s, d, z;
-	for(int i=1; i<=M; i++){
-		cin >> r >> c >> s >> d >> z;
-		if(d == 1 || d == 2) s = s % (2*R - 2);
-		else if(d == 3 || d == 4) s = s % (2*C - 2);
-		map[r][c] = z;
-		shark newShark = {r, c, s, d, z};
-		q.push(newShark);
-	}
-}
-
-
-shark moveShark(shark S){ // C^4
-	shark newShark;
-	int x, y, s, d, z;
-	x = S.x; y = S.y; s = S.s; d = S.d; z = S.z;
-	int nx = x, ny = y;
-	for(int i=1; i<=s; i++){
-		if(d == 3|| d == 4) {
-			ny += dy[d];
-			if(ny > C){
-				ny = C-1;
-				d = 4;
-			} else if(ny < 1) {
-				ny = 2;
-				d = 3;
-			}
-		} else if(d == 1 || d == 2){
-			nx += dx[d];
-			if(nx > R) {
-				nx = R-1;
-				d = 1;
-			} else if(nx < 1) {
-				nx = 2;
-				d = 2;
-			}
-		} 
-	}
-
-	if(temp[nx][ny] < z){
-		temp[nx][ny] = z;
-		newShark = {nx, ny, s, d, z};
-		return newShark;
-	}
-	return newShark;
+void catchShark(int y){
+    for(int x=1; x<=N; x++){
+        if(map[x][y]) {
+            for(int j=0; j<sharks.size(); j++) {
+                if(sharks[j].die) continue;
+                if(sharks[j].x == x && sharks[j].y == y) {
+                    sharks[j].die = true;
+                    ans+=sharks[j].z;
+                    return;
+                }
+            }
+        }
+    }
+    return;
 }
